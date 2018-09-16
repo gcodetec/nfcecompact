6c2fd21d9e11791255f76df62516291b33250375
George Moura <gwmoura@gmail.com>
2018-09-16 20:19:34 -0300

    remove all code to nfecompact pkg and add zip file to gitignore

--- main.go
+++ main.go
@@ -3,54 +3,20 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"os"
-	"strings"
+	"time"
+
+	"github.com/gcodetec/nfcecompact/nfcecompact"
 )
 
 func main() {
 	fmt.Println("listing files")
+	now := time.Now().Local()
+	currentYear := now.Year()
+	currentMonth := int(now.Month())
 	path := flag.String("path", "./data", "o diretorio dos arquivos")
+	competenceYear := flag.Int("year", currentYear, "o diretorio dos arquivos")
+	competenceMonth := flag.Int("month", currentMonth, "o diretorio dos arquivos")
 	flag.Parse()
 
-	files, err := ioutil.ReadDir(*path)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	for _, f := range files {
-		if isNfeFile(f) {
-			year, month, _ := f.ModTime().Date()
-			fmt.Println(f.Name(), year, int(month))
-			yearPath := fmt.Sprintf("%s/%v", *path, year)
-			createPath(yearPath)
-			monthPath := fmt.Sprintf("%s/%v/%v", *path, year, int(month))
-			createPath(monthPath)
-			filePath := fmt.Sprintf("%s/%s", *path, f.Name())
-			newFilePath := fmt.Sprintf("%s/%s", monthPath, f.Name())
-			moveFileToPath(filePath, newFilePath)
-		}
-	}
-}
-
-func isNfeFile(file os.FileInfo) bool {
-	if !file.IsDir() {
-		return strings.Contains(file.Name(), "-nfe")
-	}
-	return false
-}
-
-func createPath(path string) {
-	if _, err := os.Stat(path); os.IsNotExist(err) {
-		os.Mkdir(path, 0755)
-	}
-}
-
-func moveFileToPath(oldPath, newPath string) {
-	fmt.Printf("Moving file %v to %v\n", oldPath, newPath)
-	err := os.Rename(oldPath, newPath)
-	if err != nil {
-		panic(err)
-	}
+	nfcecompact.CompactFilesByCompetence(*path, *competenceYear, *competenceMonth)
 }
